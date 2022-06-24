5c9a18bc7aa84175c3acfe03d1f298356ff68fb5
djelaletdin <jelaletdinov@gmail.com>
2022-06-24 10:19:04 +0900

    Initial Commit

---
+++ Memorize-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize-SwiftUI
+//
+//  Created by Didar Jelaletdinov on 2022/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize-SwiftUI/Memorize_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_SwiftUIApp.swift
+//  Memorize-SwiftUI
+//
+//  Created by Didar Jelaletdinov on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
